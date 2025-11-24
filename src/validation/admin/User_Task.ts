9bca204e353d9728162e897cf7130bc3ac6b0402
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-11-24 16:30:18 +0200

    update validation

--- src/validation/admin/User_Task.ts
+++ src/validation/admin/User_Task.ts
@@ -4,5 +4,5 @@ export const createUserTaskSchema = Joi.object({
     user_id: Joi.string().required(),
     task_id: Joi.string().required(),
     role: Joi.string().valid( 'Member', 'Membercanapprove'),
-    user_taskId: Joi.array().items(Joi.string()).optional()
+    User_taskId: Joi.array().items(Joi.string()).optional()
 });
