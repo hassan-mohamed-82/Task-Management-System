15dea0b6f0ab4826b6536a6c1374af6ed8dffe3d
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-11-25 16:28:41 +0200

    update user rejection

--- dist/src/controller/user/User_Rejection.js
+++ dist/src/controller/user/User_Rejection.js
@@ -6,18 +6,47 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getUserRejectionById = exports.getuserRejection = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
 const BadRequest_1 = require("../../Errors/BadRequest");
+const NotFound_1 = require("../../Errors/NotFound");
 const response_1 = require("../../utils/response");
 const User_Rejection_1 = require("../../models/schema/User_Rejection");
 const getuserRejection = async (req, res) => {
     const userId = req.user?._id;
     if (!userId)
         throw new BadRequest_1.BadRequest("User not authenticated");
-    // التأكد من تحويل الـ userId لـ ObjectId
     const objectUserId = new mongoose_1.default.Types.ObjectId(userId);
-    const userRejection = await User_Rejection_1.UserRejectedReason.find({ userId: objectUserId })
-        .populate("reasonId", "reason points") // سبب الرفض ونقاطه
-        .populate("userId", "name email photo") // بيانات المستخدم
-        .populate("taskId", "name priority status start_date end_date"); // بيانات المهمة
+    const userRejection = await User_Rejection_1.UserRejectedReason.aggregate([
+        { $match: { userId: objectUserId } },
+        // Populate reasonId
+        {
+            $lookup: {
+                from: "rejectedresons",
+                localField: "reasonId",
+                foreignField: "_id",
+                as: "reasonId"
+            }
+        },
+        { $unwind: "$reasonId" },
+        // Populate userId
+        {
+            $lookup: {
+                from: "users",
+                localField: "userId",
+                foreignField: "_id",
+                as: "userId"
+            }
+        },
+        { $unwind: "$userId" },
+        // Populate taskId
+        {
+            $lookup: {
+                from: "tasks",
+                localField: "taskId",
+                foreignField: "_id",
+                as: "taskId"
+            }
+        },
+        { $unwind: "$taskId" }
+    ]);
     (0, response_1.SuccessResponse)(res, {
         message: "User rejection records retrieved successfully",
         userRejection
@@ -25,12 +54,29 @@ const getuserRejection = async (req, res) => {
 };
 exports.getuserRejection = getuserRejection;
 const getUserRejectionById = async (req, res) => {
-    const user = req.user?._id;
-    const id = req.params.id;
-    if (!id) {
-        throw new BadRequest_1.BadRequest("Please provide user id");
+    const userId = req.user?._id;
+    const { id } = req.params;
+    if (!id)
+        throw new BadRequest_1.BadRequest("Rejection ID is required");
+    // Validate ID
+    if (!mongoose_1.default.Types.ObjectId.isValid(id)) {
+        throw new BadRequest_1.BadRequest("Invalid rejection ID");
     }
-    const userRejection = await User_Rejection_1.UserRejectedReason.findById(id).populate("reasonId", "reason points").populate("userId", "name email photo").populate("taskId", "name priority status startDate endDate");
-    (0, response_1.SuccessResponse)(res, { message: "user Rejection", userRejection });
+    // Fetch rejection record
+    const rejection = await User_Rejection_1.UserRejectedReason.findById(id)
+        .populate("reasonId", "reason points")
+        .populate("userId", "name email photo")
+        .populate("taskId", "name priority status start_date end_date");
+    if (!rejection) {
+        throw new NotFound_1.NotFound("Rejection record not found");
+    }
+    // Make sure this rejection belongs to the logged-in user
+    if (rejection.userId.toString() !== userId?.toString()) {
+        throw new BadRequest_1.BadRequest("You are not allowed to view this rejection record");
+    }
+    return (0, response_1.SuccessResponse)(res, {
+        message: "Rejection record retrieved successfully",
+        userRejection: rejection
+    });
 };
 exports.getUserRejectionById = getUserRejectionById;
--- src/controller/user/User_Rejection.ts
+++ src/controller/user/User_Rejection.ts
@@ -9,13 +9,44 @@ export const getuserRejection = async (req: Request, res: Response) => {
   const userId = req.user?._id;
   if (!userId) throw new BadRequest("User not authenticated");
 
-  // التأكد من تحويل الـ userId لـ ObjectId
   const objectUserId = new mongoose.Types.ObjectId(userId);
 
-  const userRejection = await UserRejectedReason.find({ userId: objectUserId })
-    .populate("reasonId", "reason points")   // سبب الرفض ونقاطه
-    .populate("userId", "name email photo")   // بيانات المستخدم
-    .populate("taskId", "name priority status start_date end_date"); // بيانات المهمة
+  const userRejection = await UserRejectedReason.aggregate([
+    { $match: { userId: objectUserId } },
+
+    // Populate reasonId
+    {
+      $lookup: {
+        from: "rejectedresons",
+        localField: "reasonId",
+        foreignField: "_id",
+        as: "reasonId"
+      }
+    },
+    { $unwind: "$reasonId" },
+
+    // Populate userId
+    {
+      $lookup: {
+        from: "users",
+        localField: "userId",
+        foreignField: "_id",
+        as: "userId"
+      }
+    },
+    { $unwind: "$userId" },
+
+    // Populate taskId
+    {
+      $lookup: {
+        from: "tasks",
+        localField: "taskId",
+        foreignField: "_id",
+        as: "taskId"
+      }
+    },
+    { $unwind: "$taskId" }
+  ]);
 
   SuccessResponse(res, {
     message: "User rejection records retrieved successfully",
@@ -24,11 +55,33 @@ export const getuserRejection = async (req: Request, res: Response) => {
 };
 
 export const getUserRejectionById = async (req: Request, res: Response) => {
-  const user = req.user?._id;
- const id= req.params.id;
- if (!id) {
-   throw new BadRequest("Please provide user id");
- }
-  const userRejection = await UserRejectedReason.findById(id).populate("reasonId","reason points").populate("userId","name email photo").populate("taskId","name priority status startDate endDate");
-  SuccessResponse(res,{message: "user Rejection", userRejection});
-}
+  const userId = req.user?._id;
+  const { id } = req.params;
+
+  if (!id) throw new BadRequest("Rejection ID is required");
+
+  // Validate ID
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw new BadRequest("Invalid rejection ID");
+  }
+
+  // Fetch rejection record
+  const rejection = await UserRejectedReason.findById(id)
+    .populate("reasonId", "reason points")
+    .populate("userId", "name email photo")
+    .populate("taskId", "name priority status start_date end_date");
+
+  if (!rejection) {
+    throw new NotFound("Rejection record not found");
+  }
+
+  // Make sure this rejection belongs to the logged-in user
+  if (rejection.userId.toString() !== userId?.toString()) {
+    throw new BadRequest("You are not allowed to view this rejection record");
+  }
+
+  return SuccessResponse(res, {
+    message: "Rejection record retrieved successfully",
+    userRejection: rejection
+  });
+};
