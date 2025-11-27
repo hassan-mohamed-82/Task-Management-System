66b6bd0ff7810deee0beb8aa1a4d2c2450d823ef
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-11-27 13:32:49 +0200

    last update

--- dist/src/utils/sendEmails.js
+++ dist/src/utils/sendEmails.js
@@ -25,26 +25,7 @@ const sendEmail = async (to, subject, text) => {
         console.log(" Email sent:", info.response);
     }
     catch (error) {
-        console.error("❌ Email error:", error);
+        console.error(" Email error:", error);
     }
 };
 exports.sendEmail = sendEmail;
-// import SibApiV3Sdk from "sib-api-v3-sdk";
-// // إعداد Brevo API
-// const client = SibApiV3Sdk.ApiClient.instance;
-// client.authentications["api-key"].apiKey = process.env.BREVO_API_KEY!;
-// const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-// // دالة لإرسال الإيميل
-// export const sendEmail = async (to: string, subject: string, text: string) => {
-//   try {
-//     await apiInstance.sendTransacEmail({
-//       sender: { email: process.env.BREVO_SENDER_EMAIL!, name: "Smart College" },
-//       to: [{ email: to }],
-//       subject,
-//       textContent: text,
-//     });
-//     console.log("✅ Email sent via Brevo API");
-//   } catch (error) {
-//     console.error("❌ Email error:", error);
-//   }
-// };
--- src/utils/sendEmails.ts
+++ src/utils/sendEmails.ts
@@ -21,29 +21,6 @@ export const sendEmail = async (to: string, subject: string, text: string) => {
     });
     console.log(" Email sent:", info.response);
   } catch (error) {
-    console.error("❌ Email error:", error);
+    console.error(" Email error:", error);
   }
 };
-
-// import SibApiV3Sdk from "sib-api-v3-sdk";
-
-// // إعداد Brevo API
-// const client = SibApiV3Sdk.ApiClient.instance;
-// client.authentications["api-key"].apiKey = process.env.BREVO_API_KEY!;
-
-// const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-
-// // دالة لإرسال الإيميل
-// export const sendEmail = async (to: string, subject: string, text: string) => {
-//   try {
-//     await apiInstance.sendTransacEmail({
-//       sender: { email: process.env.BREVO_SENDER_EMAIL!, name: "Smart College" },
-//       to: [{ email: to }],
-//       subject,
-//       textContent: text,
-//     });
-//     console.log("✅ Email sent via Brevo API");
-//   } catch (error) {
-//     console.error("❌ Email error:", error);
-//   }
-// };
