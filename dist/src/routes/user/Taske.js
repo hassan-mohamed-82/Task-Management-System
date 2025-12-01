b92fdf945c3f97d087657f5ef2c461c3bed0cb49
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-12-01 15:36:27 +0200

    update task

--- dist/src/routes/user/Taske.js
+++ dist/src/routes/user/Taske.js
@@ -4,9 +4,12 @@ const express_1 = require("express");
 const taske_1 = require("../../controller/user/taske");
 const catchAsync_1 = require("../../utils/catchAsync");
 const route = (0, express_1.Router)();
+// 1) routes الثابتة الأول
+route.get("/selection", (0, catchAsync_1.catchAsync)(taske_1.selection));
+// 2) routes الخاصة بالمراجعة / تفاصيل التاسك
+route.put("/review/:taskId", (0, catchAsync_1.catchAsync)(taske_1.reviewUserTaskByApprover));
+route.get("/task/:taskId", (0, catchAsync_1.catchAsync)(taske_1.getUserTaskByTaskId));
+// 3) في الآخر الـ routes الديناميكية العامة
 route.put("/:taskId", (0, catchAsync_1.catchAsync)(taske_1.updateUserTaskStatus));
 route.get("/:taskId", (0, catchAsync_1.catchAsync)(taske_1.getalltaskatprojectforuser));
-route.get("/task/:taskId", (0, catchAsync_1.catchAsync)(taske_1.getUserTaskByTaskId));
-route.put("/review/:taskId", (0, catchAsync_1.catchAsync)(taske_1.reviewUserTaskByApprover));
-route.get("/selection", (0, catchAsync_1.catchAsync)(taske_1.selection));
 exports.default = route;
--- src/routes/user/Taske.ts
+++ src/routes/user/Taske.ts
@@ -1,17 +1,37 @@
 import { Router } from "express";
-import {updateUserTaskStatus,getalltaskatprojectforuser,getUserTaskByTaskId,reviewUserTaskByApprover,selection} from "../../controller/user/taske"
+import {
+  updateUserTaskStatus,
+  getalltaskatprojectforuser,
+  getUserTaskByTaskId,
+  reviewUserTaskByApprover,
+  selection,
+} from "../../controller/user/taske";
 import { catchAsync } from "../../utils/catchAsync";
 import { checkProjectOrTaskRole } from "../../middlewares/authorized";
+
 const route = Router();
-route.put("/:taskId", 
-    catchAsync(updateUserTaskStatus));
-route.get("/:taskId" ,
-    catchAsync(getalltaskatprojectforuser));
-route.get("/task/:taskId", 
-    catchAsync(getUserTaskByTaskId));
-route.put("/review/:taskId", 
-    catchAsync(reviewUserTaskByApprover));
+
+// 1) routes الثابتة الأول
 route.get("/selection",
-    catchAsync(selection));
+  catchAsync(selection)
+);
+
+// 2) routes الخاصة بالمراجعة / تفاصيل التاسك
+route.put("/review/:taskId",
+  catchAsync(reviewUserTaskByApprover)
+);
+
+route.get("/task/:taskId",
+  catchAsync(getUserTaskByTaskId)
+);
+
+// 3) في الآخر الـ routes الديناميكية العامة
+route.put("/:taskId",
+  catchAsync(updateUserTaskStatus)
+);
+
+route.get("/:taskId",
+  catchAsync(getalltaskatprojectforuser)
+);
 
 export default route;
