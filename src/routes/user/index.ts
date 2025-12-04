62fffabba1d1a1cc4d1bfc23712851e295a7aeaa
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-12-04 15:21:07 +0200

    refactor: reorder middleware usage for better route organization

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -12,11 +12,11 @@ import { authorizeRoles } from "../../middlewares/authorized";
 import { checkProjectOrTaskRole } from "../../middlewares/authorized";
 const route = Router();
 route.use("/auth", authRouter);
-route.use(authenticated, authorizeRoles('user','admin'));
+route.use("/plans", plansRouter);
 route.use("/payment-methods", paymentMethodRouter);
+route.use(authenticated, authorizeRoles('user','admin'));
 route.use("/payments", paymentRouter);
 route.use("/profile", profileRouter);
-route.use("/plans", plansRouter);
 route.use("/user-rejections", userRejectionRouter);
 route.use("/projects",
     ProjectRouter);
